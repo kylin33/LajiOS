b6cbd46d0f6c4112d829b220d29c250612797fa4
kylin33 <kylin33@outlook.com>
2025-09-30 18:17:35 +0800

    增加TSS

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -20,8 +20,9 @@ pub fn init_idt(){
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
-    stack_frame: InterruptStackFrame){
-        println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+    stack_frame: InterruptStackFrame)
+{
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
 extern "x86-interrupt" fn double_fault_handler(
